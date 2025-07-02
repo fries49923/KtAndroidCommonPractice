23a2113b87ff6f6e34b1c50ff2abc2c2fb9ac612
ポテト <fries49923@gmail.com>
2025-07-02 22:51:24 +0800

    更換使用 ModalNavigationDrawer

--- HiltDiTest/app/src/main/java/com/example/hiltditest/MainActivity.kt
+++ HiltDiTest/app/src/main/java/com/example/hiltditest/MainActivity.kt
@@ -6,25 +6,36 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.example.hiltditest.ui.theme.HiltDiTestTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -54,27 +65,72 @@ fun MainScreenPage(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen(
     page01Count: Int,
     page01OnIncrement: () -> Unit
 ) {
-    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = rememberCoroutineScope()
+    var selectedPage by remember { mutableIntStateOf(0) }
 
-    Scaffold(
-        bottomBar = {
-            BottomNavigationBar(
-                selectedTabIndex = selectedTabIndex,
-                onTabSelected = { selectedTabIndex = it }
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet {
+
+                // 往下推一點空間
+                Spacer(modifier = Modifier.height(100.dp))
+
+                // Page01
+                NavigationDrawerItem(
+                    label = { Text("Page01") },
+                    selected = selectedPage == 0,
+                    onClick = {
+                        selectedPage = 0
+                        scope.launch { drawerState.close() }
+                    }
+                )
+
+                // Page02
+                NavigationDrawerItem(
+                    label = { Text("Page02") },
+                    selected = selectedPage == 1,
+                    onClick = {
+                        selectedPage = 1
+                        scope.launch { drawerState.close() }
+                    }
+                )
+                // 擴充更多項目
+            }
+        }
+    ) {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("App title") },
+                    navigationIcon = {
+                        IconButton(
+                            onClick = {
+                                scope.launch { drawerState.open() }
+                            }) {
+                            Icon(
+                                Icons.Default.Menu,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
+        ) { padding ->
+            ScreenContainer(
+                page01Count = page01Count,
+                page01OnIncrement = page01OnIncrement,
+                selectedPage = selectedPage,
+                modifier = Modifier.padding(padding)
             )
         }
-    ) { padding ->
-        ScreenContainer(
-            page01Count = page01Count,
-            page01OnIncrement = page01OnIncrement,
-            selectedTabIndex = selectedTabIndex,
-            Modifier.padding(padding)
-        )
     }
 }
 
@@ -82,58 +138,17 @@ fun MainScreen(
 fun ScreenContainer(
     page01Count: Int,
     page01OnIncrement: () -> Unit,
-    selectedTabIndex: Int,
+    selectedPage: Int,
     modifier: Modifier = Modifier
 ) {
     Box(modifier = modifier.fillMaxSize()) {
-        when (selectedTabIndex) {
+        when (selectedPage) {
             0 -> Page01Screen(
                 count = page01Count,
                 onIncrement = page01OnIncrement
             )
             //1 -> Page2Screen()
         }
-    }
-}
-
-@Composable
-private fun BottomNavigationBar(
-    selectedTabIndex: Int,
-    onTabSelected: (Int) -> Unit,
-    modifier: Modifier = Modifier
-) {
-    NavigationBar(
-        modifier = modifier
-    ) {
-        // Page01
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text("Page01")
-            },
-            selected = selectedTabIndex == 0,
-            onClick = { onTabSelected(0) },
-        )
-
-        // Page02
-        NavigationBarItem(
-            icon = {
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = null
-                )
-            },
-            label = {
-                Text("Page02")
-            },
-            selected = selectedTabIndex == 1,
-            onClick = { onTabSelected(1) },
-        )
     }
 }
 
